788362e0d0649501fc5d9a256a07131ef09d38fc
Xynnix <zkjcksn@gmail.com>
2022-01-27 23:34:39 -0500

    Added /image

--- utils/register.js
+++ utils/register.js
@@ -4,22 +4,45 @@ dotenv.config();
 import { INVITE_COMMAND, NSFW_COMMAND, IMAGE_COMMAND } from "./commands.js";
 
 const guildId = process.env.GUILDID;
-const response = await fetch(
-  `https://discord.com/api/v9/applications/${process.env.APPLICATION_ID}/guilds/${process.env.GUILDID}/commands`,
-  {
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bot ${process.env.TOKEN}`,
-    },
-    method: "PUT",
-    body: JSON.stringify([NSFW_COMMAND, INVITE_COMMAND, IMAGE_COMMAND]),
-  }
-);
+if (guildId && process.env.guildOnly == "true") {
+  const response = await fetch(
+    `https://discord.com/api/v9/applications/${process.env.APPLICATION_ID}/guilds/${process.env.GUILDID}/commands`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bot ${process.env.TOKEN}`,
+      },
+      method: "PUT",
+      body: JSON.stringify([NSFW_COMMAND, INVITE_COMMAND, IMAGE_COMMAND]),
+    }
+  );
 
-if (response.ok) {
-  console.log("Registered all commands");
+  if (response.ok) {
+    console.log("Registered all guild commands");
+  } else {
+    console.error("Error registering commands");
+    const text = await response.text();
+    console.error(text);
+  }
 } else {
-  console.error("Error registering commands");
-  const text = await response.text();
-  console.error(text);
+  const response = await fetch(
+    // `https://discord.com/api/v9/applications/${process.env.APPLICATION_ID}/guilds/${process.env.GUILDID}/commands`,
+    `https://discord.com/api/v9/applications/${process.env.APPLICATION_ID}/commands`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bot ${process.env.TOKEN}`,
+      },
+      method: "PUT",
+      body: JSON.stringify([NSFW_COMMAND, INVITE_COMMAND, IMAGE_COMMAND]),
+    }
+  );
+
+  if (response.ok) {
+    console.log("Registered all global commands");
+  } else {
+    console.error("Error registering commands");
+    const text = await response.text();
+    console.error(text);
+  }
 }
