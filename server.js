8eed271eb9048e91dd37044339a57cfc453896e0
Night Raven <zkjcksn@gmail.com>
2022-04-07 01:17:36 -0400

    Change endpoints to app id

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ server.register(rawBody, {
   runFirst: true,
 });
 
-server.get('/api/interactions', (request, response) => {
+server.get(`/api/interactions/`+process.env.APPLICATION_ID, (request, response) => {
   console.log('Handling GET request');
 });
 const embed = new MessageEmbed()
@@ -46,7 +46,7 @@ server.addHook('preHandler', async (request, response) => {
     }
   }
 });
-server.post('/api/interactions', async (request, response) => {
+server.post(`/api/interactions/`+process.env.APPLICATION_ID, async (request, response) => {
   const message = request.body;
   if (message.type === InteractionType.PING) {
     console.log('Handling Ping request');
@@ -131,7 +131,6 @@ process.on("unhandledException", (err) => {
 server.listen(process.env.PORT, async (error, address) => {
   if (error) {
     server.log.error(error);
-    //    process.exit(1);
   }
   server.log.info(`server listening on ${address}`);
 });
@@ -158,16 +157,3 @@ function member(guildId, userId) {
     return json
   });
 }
-
-import { Client } from "discord.js";
-const bots = new Client({ presence: { status: "invisible" }, intents: ['GUILDS'] });
-const replies = ["https://cdn.discordapp.com/attachments/763593786118504529/939641798802944051/2.jpg", "https://cdn.discordapp.com/attachments/763593786118504529/939641798450626680/1.jpg"]
-
-bots.on("ready", () => {
-  setInterval(() => {
-    let avatarURL = Math.floor((Math.random() * replies.length));
-    bots.user.setAvatar(replies[avatarURL]);
-    console.log(replies[avatarURL])
-  }, 1800000)
-})
-bots.login(process.env.TOKEN)
